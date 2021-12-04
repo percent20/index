43bf03029853457072b549847abc61656062b790
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2021-12-04 22:02:44 +0000

    Add script to soft link openpnp files on Linux

---
+++ link-openpnp.sh
@@ -0,0 +1,4 @@
+# Create soft links for OpenPnP files on Linux
+ln -n ~/.openpnp2/packages.xml openpnp/packages.xml
+ln -n ~/.openpnp2/parts.xml openpnp/parts.xml
+ln -n ~/.openpnp2/machine.xml openpnp/machine.xml
